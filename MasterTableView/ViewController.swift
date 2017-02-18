146920b170b5b11a8e24fbf984be08fda0d7b612
DesmondDAI <dxmlore@gmail.com>
2017-02-18 17:43:45 +0800

    Initial Commit

---
+++ MasterTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasterTableView
+//
+//  Created by DAIXinming on 18/02/2017.
+//  Copyright © 2017 Xinming DAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
